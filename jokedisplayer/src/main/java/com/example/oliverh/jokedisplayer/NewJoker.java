ba03286f1d870f960e1c0b35f5890e852465c6be
jhlink <jhlink@users.noreply.github.com>
2019-02-03 18:13:15 -0500

    feat: Show joke text in TextView (NewJoker)

--- jokedisplayer/src/main/java/com/example/oliverh/jokedisplayer/NewJoker.java
+++ jokedisplayer/src/main/java/com/example/oliverh/jokedisplayer/NewJoker.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class NewJoker extends AppCompatActivity {
 
@@ -13,17 +14,12 @@ public class NewJoker extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newjoke);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        String newJokeString = getIntent().getStringExtra(getString(R.string.newJokeKey));
+        TextView jokeTv = findViewById(R.id.tv_newJoke);
+        jokeTv.setText(newJokeString);
     }
-
 }
