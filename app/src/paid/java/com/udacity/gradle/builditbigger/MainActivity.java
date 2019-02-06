f8f5dedfd4dc4fe0e67c07b8fc300189a1190093
jhlink <jhlink@users.noreply.github.com>
2019-02-05 20:59:21 -0500

    fix: Remove irrelevant params for EndpointAsyncTask request

--- app/src/paid/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -41,6 +41,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Manfred"));
+        new EndpointsAsyncTask().execute(this);
     }
 }
