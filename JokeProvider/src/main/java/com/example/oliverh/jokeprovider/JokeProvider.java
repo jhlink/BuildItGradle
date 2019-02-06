547c10419c4391aa451c63dd3070caec053fa306
jhlink <jhlink@users.noreply.github.com>
2019-02-05 19:30:00 -0500

    feat: Extract jokeList initialization into method

--- JokeProvider/src/main/java/com/example/oliverh/jokeprovider/JokeProvider.java
+++ JokeProvider/src/main/java/com/example/oliverh/jokeprovider/JokeProvider.java
@@ -9,24 +9,27 @@ public class JokeProvider {
 
     JokeProvider() {
         if ( jokeList == null )  {
-            jokeList = new ArrayList<>();
-            jokeList.add(new Joke("What did the baby digital watch say to the mommy analog watch?",
-                            "Look Ma, no hands!"));
-            jokeList.add(new Joke("Why did the crab never share?",
-                    "Because he's shellfish!"));
-            jokeList.add(new Joke("What's the smartest animal?",
-                    "A fish because they stay in schools!"));
-            jokeList.add(new Joke("How much money does a skunk have?",
-                    "One scent!"));
-            jokeList.add(new Joke("What vegetables do librarians like?",
-                    "Quiet peas."));
-            jokeList.add(new Joke("Why didn't the sun go to college?",
-                    "Because it already had a million degrees!"));
-            jokeList.add(new Joke("What do you call friends who love math?",
-                    "Algebros"));
-
+            initializeJokeList();
             randomGenerator = new Random();
         }
+    }
+
+    private void initializeJokeList() {
+        jokeList = new ArrayList<>();
+        jokeList.add(new Joke("What did the baby digital watch say to the mommy analog watch?",
+                "Look Ma, no hands!"));
+        jokeList.add(new Joke("Why did the crab never share?",
+                "Because he's shellfish!"));
+        jokeList.add(new Joke("What's the smartest animal?",
+                "A fish because they stay in schools!"));
+        jokeList.add(new Joke("How much money does a skunk have?",
+                "One scent!"));
+        jokeList.add(new Joke("What vegetables do librarians like?",
+                "Quiet peas."));
+        jokeList.add(new Joke("Why didn't the sun go to college?",
+                "Because it already had a million degrees!"));
+        jokeList.add(new Joke("What do you call friends who love math?",
+                "Algebros"));
     }
 
     public String getJoke() {
